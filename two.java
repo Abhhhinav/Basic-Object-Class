1db9cea7bd963f9840cad434cab63b5d08bf4ef2
Abhhhinav <abhinav.209301211@muj.manipal.edu>
2023-08-04 08:31:18 +0530

    classes and objects

---
+++ two.java
@@ -0,0 +1,19 @@
+public class two {
+    public static void main(String args[]) {
+        Pen p1 = new Pen();
+        p1.setColor("Orange");
+        System.out.println(p1.color);
+    }
+}
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
