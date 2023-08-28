6b0c55391871dcc6220868b7b6f5fb3a08a4cde6
Abhhhinav <abhinav.209301211@muj.manipal.edu>
2023-08-28 23:18:26 +0530

    FInal

--- one.java
+++ one.java
@@ -1,25 +1,28 @@
 class Pen {
-    String color;
-    String type;
-      public void write(){
-        System.out.println("XYZ");
-    }
-      public void colorrr(){
-        System.out.println(this.color);
-    }
+  String color;
+  String type;
+
+  public void write() {
+    System.out.println("XYZ");
+  }
+
+  public void colorrr() {
+    System.out.println(this.color);
+  }
 }
 
-public class one{
-    public static void main(String[] args) {
-        Pen A = new Pen();
-        A.color = "blue";
-        A.type = "gel";
+public class one {
+  public static void main(String[] args) {
+    Pen A = new Pen();
+    A.color = "blue";
+    A.type = "gel";
 
-        Pen B = new Pen();
-        B.color = "Black";
-        B.type = "Ball-pen" ;
+    Pen B = new Pen();
+    B.color = "Black";
+    B.type = "Ball-pen";
 
-        A.colorrr();
-        B.colorrr();
-    }
+    A.colorrr();
+    B.colorrr();
+  }
 }
+
